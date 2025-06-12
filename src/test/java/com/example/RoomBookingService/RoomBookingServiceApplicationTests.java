d2b2e87b07a6d77aefd30fb5c4f84bbf9024797b
Gregor Uusväli <gregor.uusvali@err.ee>
2025-06-12 09:56:27 +0300

    init

---
+++ src/test/java/com/example/RoomBookingService/RoomBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoomBookingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
